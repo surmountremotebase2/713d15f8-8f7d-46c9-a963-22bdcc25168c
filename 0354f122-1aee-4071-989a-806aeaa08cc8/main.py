5b5ea79a03339593ab7672c7b30677737e6e879e
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-11-12 14:24:16 -0700

    User Daniel Woodard saved changes to main.py at 11/12/2024, 21:24:15 UTC time.

--- 0354f122-1aee-4071-989a-806aeaa08cc8/main.py
+++ 0354f122-1aee-4071-989a-806aeaa08cc8/main.py
@@ -44,7 +44,7 @@ class TradingStrategy(Strategy):
 
     @property
     def assets(self):
-        return ["SPY", "QQQ", "AAPL", "GOOGL"]  # Add your desired assets
+        return ["AAPL"]  # Add your desired assets
 
     def run(self, data):
         allocation_dict = {ticker: 0 for ticker in self.assets}
