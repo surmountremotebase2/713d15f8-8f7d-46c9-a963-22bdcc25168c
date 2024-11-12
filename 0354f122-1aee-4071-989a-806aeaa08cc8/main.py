b973583d660256c8c61cd94c7e436d15226bc7e6
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-11-12 14:20:00 -0700

    User Daniel Woodard saved changes to main.py at 11/12/2024, 21:19:59 UTC time.

--- 0354f122-1aee-4071-989a-806aeaa08cc8/main.py
+++ 0354f122-1aee-4071-989a-806aeaa08cc8/main.py
@@ -48,7 +48,7 @@ def SAM(ticker, data, cc_length=8, median_length=8, smooth_length=8):
 
     return sam.tolist()
 
-class MyStrategy(Strategy):
+class TradingStrategy(Strategy):
     @property
     def interval(self):
         return "1day"
